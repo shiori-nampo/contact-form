32fe4be0601f3246c71cc7f76d06ffdde42c07e1
shiori0301 <st.ryo.9805@icloud.com>
2025-11-15 10:10:04 +0900

    Add validation/feature

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -30,10 +30,12 @@
 </div>
 <div class="form__group-content">
     <div class="form__input--text">
-    <input type="text" name="name" placeholder="テスト太郎">
+    <input type="text" name="name" placeholder="テスト太郎" value="{{ old('name') }}" />
 </div>
     <div class="form__error">
-        <!--バリデーション-->
+        @error('name')
+        {{ $message }}
+        @enderror
     </div>
 </div>
 </div>
@@ -44,10 +46,12 @@
 </div>
 <div class="form__group-content">
     <div class="form__input--text">
-    <input type="email" name="email" placeholder="test@example.com">
+    <input type="email" name="email" placeholder="test@example.com" value="{{ old('email') }}"/>
 </div>
     <div class="form__error">
-        <!--validation-->
+        @error('email')
+        {{ $message }}
+        @enderror
     </div>
 </div>
 </div>
@@ -58,10 +62,12 @@
 </div>
 <div class="form__group-content">
     <div class="form__input--text">
-                <input type="tel" name="tel" placeholder="09012345678" />
+                <input type="tel" name="tel" placeholder="09012345678" value="{{ old('tel') }}" />
 </div>
                 <div class="form__error">
-                    <!--バリデーション-->
+                    @error('tel')
+                    {{ $message }}
+                    @enderror
                 </div>
 </div>
 </div>
